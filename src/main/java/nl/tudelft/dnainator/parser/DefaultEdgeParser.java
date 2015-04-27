1585f39317fe0c6f48e6873925db2a8cd77d370a
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2015-04-27 10:55:02 +0200

    Update style and let parsers use FileInputStreams
    Most changes are style fixes/improvements.
    The only big difference is that EdgeParser and NodeParser
    use FileInputStreams no, whereas they were using Files before.
    
    Cherry-picked!

--- src/main/java/nl/tudelft/dnainator/parser/DefaultEdgeParser.java
+++ src/main/java/nl/tudelft/dnainator/parser/DefaultEdgeParser.java
@@ -1,16 +1,16 @@
 package nl.tudelft.dnainator.parser;
 
-import java.io.File;
-import java.util.Map;
-
 import nl.tudelft.dnainator.core.Sequence;
 import nl.tudelft.dnainator.core.SequenceGraph;
 
+import java.io.FileInputStream;
+import java.util.Map;
+
 public class DefaultEdgeParser implements EdgeParser {
 
-	@Override
-	public SequenceGraph parse(Map<String, Sequence> nodes, File edgeFile) {
-		return null;
-	}
+    @Override
+    public SequenceGraph parse(Map<String, Sequence> nodes, FileInputStream edgeIn) {
+        return null;
+    }
 
 }
--- src/main/java/nl/tudelft/dnainator/parser/NodeParser.java
+++ src/main/java/nl/tudelft/dnainator/parser/NodeParser.java
@@ -1,19 +1,29 @@
 package nl.tudelft.dnainator.parser;
 
-import java.io.File;
-import java.util.Map;
-
 import nl.tudelft.dnainator.core.Sequence;
 import nl.tudelft.dnainator.core.SequenceFactory;
 
+import java.io.FileInputStream;
+import java.util.Map;
+
 public abstract class NodeParser {
 
-	protected SequenceFactory sf;
+    protected SequenceFactory sf;
 
-	public NodeParser(SequenceFactory sf) {
-		this.sf = sf;
-	}
+    public NodeParser(SequenceFactory sf) {
+        this.sf = sf;
+    }
 
-	public abstract Map<String, Sequence> parse(File f)
-			throws NumberFormatException, InvalidHeaderFormatException;
+    /**
+     * Tries to parse the given input stream to a Map in which the tuples denote the
+     * header and data of the nodes.
+     * The caller is responsible for closing the input stream.
+     *
+     * @param fIn The FileInputStream to be used for parsing.
+     * @return A Map of String, Sequence tuples containing all nodes.
+     * @throws NumberFormatException
+     * @throws InvalidHeaderFormatException
+     */
+    public abstract Map<String, Sequence> parse(FileInputStream fIn)
+            throws NumberFormatException, InvalidHeaderFormatException;
 }
