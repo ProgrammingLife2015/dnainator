b6d800460663da02baad75a95e4bec7d3f1db1fa
Jente Hidskes <hjdskes@gmail.com>
2015-05-08 21:51:22 +0200

    Draw complete graphs

--- src/main/java/nl/tudelft/dnainator/ui/views/SequenceView.java
+++ src/main/java/nl/tudelft/dnainator/ui/views/SequenceView.java
@@ -1,9 +1,14 @@
 package nl.tudelft.dnainator.ui.views;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javafx.geometry.Point2D;
+import nl.tudelft.dnainator.core.Edge;
 import nl.tudelft.dnainator.core.SequenceNode;
+import nl.tudelft.dnainator.graph.Neo4jSingleton;
+import nl.tudelft.dnainator.ui.drawables.DrawableEdge;
 import nl.tudelft.dnainator.ui.drawables.DrawableNode;
 import nl.tudelft.dnainator.ui.layouts.TopoLayout;
 import nl.tudelft.dnainator.ui.models.GraphModel;
@@ -13,6 +18,7 @@ import nl.tudelft.dnainator.ui.models.GraphModel;
  * it displays the sequences of the DNA.
  */
 public class SequenceView extends View {
+	private Map<String, DrawableNode> drawableNodes;
 
 	/**
 	 * Instantiates a new SequenceView object.
@@ -21,17 +27,39 @@ public class SequenceView extends View {
 	public SequenceView(GraphModel model) {
 		super(model);
 
+		drawableNodes = new HashMap<>();
+
 		getStyleClass().add("sequence-view");
 	}
 
 	@Override
 	public void redraw() {
 		System.out.println("Redrawing...");
+		//drawGraph();
 
 		drawNodes();
-		//drawEdges();
+		drawEdges();
+
+		group.getChildren().addAll(drawableNodes.values());
 		System.out.println("Done!");
 	}
+
+//	private void drawGraph() {
+//		List<Edge<String>> edges = Neo4jSingleton.getInstance().getDatabase().getEdges();
+//		for (Edge<String> e : edges) {
+//			SequenceNode srcNode = e.getSource();
+//			SequenceNode dstNode = e.getDest();
+//
+//			Point2D srcCoords = layout.transform(srcNode.getRank());
+//			Point2D dstCoords = layout.transform(dstNode.getRank());
+//
+//			DrawableNode src = new DrawableNode(srcNode, srcCoords.getX(), srcCoords.getY());
+//			DrawableNode dst = new DrawableNode(dstNode, dstCoords.getX(), dstCoords.getY());
+//			DrawableEdge de = new DrawableEdge(src, dst);
+//
+//			group.getChildren().addAll(src, dst, de);
+//		}
+//	}
 
 	private void drawNodes() {
 		List<List<SequenceNode>> ranks = model.getRanks();
@@ -43,20 +71,20 @@ public class SequenceView extends View {
 				SequenceNode n = nodes.get(j);
 
 				DrawableNode drawable = new DrawableNode(n, coords.getX(), coords.getY());
-				group.getChildren().add(drawable);
+				drawableNodes.put(n.getId(), drawable);
 			}
 		}
 	}
 
-//	private void drawEdges() {
-//		double x = 50.0, y = 50.0;
-//
-//		for (Edge<Integer> e : model.getEdges()) {
-//			DrawableNode src = (DrawableNode) root.getChildren().get(e.getSource());
-//			DrawableNode dst = (DrawableNode) root.getChildren().get(e.getDest());
-//			root.getChildren().add(new DrawableEdge(src, dst));
-//			x += 50.0;
-//			y += 50.0;
-//		}
-//	}
+	private void drawEdges() {
+		for (Edge<String> e : Neo4jSingleton.getInstance().getDatabase().getEdges()) {
+			DrawableNode src = drawableNodes.get(e.getSource());
+			DrawableNode dst = drawableNodes.get(e.getDest());
+			if (src == null || dst == null) {
+				continue;
+			}
+			DrawableEdge de = new DrawableEdge(src, dst);
+			group.getChildren().add(de);
+		}
+	}
 }
