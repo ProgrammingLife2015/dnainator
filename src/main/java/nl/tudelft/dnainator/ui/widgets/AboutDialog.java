978a2d7dedec072d9fa81f2499dbbb411ed049bb
Owen <0w3nhuang@gmail.com>
2015-05-13 21:21:25 +0200

    Read correct version from properties

--- src/main/java/nl/tudelft/dnainator/ui/widgets/AboutDialog.java
+++ src/main/java/nl/tudelft/dnainator/ui/widgets/AboutDialog.java
@@ -1,5 +1,9 @@
 package nl.tudelft.dnainator.ui.widgets;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
@@ -13,9 +17,11 @@ import javafx.stage.Stage;
  */
 public class AboutDialog {
 	private Dialog<String> dialog;
+	private Properties prop;
 	private static final String ICON = "/ui/icons/dnainator128x128.png";
 	private static final String STYLE = "/ui/style.css";
-
+	private static final String PROPERTIES = "config.properties";
+	
 	/**
 	 * Instantiates a new AboutDialog.
 	 * It will set up all the necessities for displaying 
@@ -36,6 +42,7 @@ public class AboutDialog {
 		ImageView img = new ImageView();
 		img.setImage(icon);
 		dialog.setGraphic(img);
+		readProperties();
 		
 		dialog.setHeaderText("DNAinator\nDNA network visualization tool");
 		dialog.setContentText(contentText());
@@ -46,11 +53,23 @@ public class AboutDialog {
 
 	private String contentText() {
 		StringBuilder contents = new StringBuilder();
+		contents.append("Version: " + prop.getProperty("version") + "\n");
 		contents.append("Jente Hidskes, Gerlof Fokkema, Owen Huang, "
 				+ "Skip Lentz, Piet van Agtmaal");
 		return contents.toString();
 	}
 	
+	private void readProperties() {
+		prop = new Properties();
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES);
+		if (inputStream != null) {
+			try {
+				prop.load(inputStream);
+			} catch (IOException e) {
+				new ExceptionDialog(e, "Error reading properties");
+			}
+		}
+	}
 
 	/**
 	 * Shows the {@link dialog} if it is not null.
