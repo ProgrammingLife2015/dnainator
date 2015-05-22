a59768245ba1e13968d973156d64a5021ea2b44f
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2015-05-22 16:31:05 +0200

    Allow for arrow key scrolling

--- src/main/java/nl/tudelft/dnainator/ui/controllers/ViewController.java
+++ src/main/java/nl/tudelft/dnainator/ui/controllers/ViewController.java
@@ -3,6 +3,7 @@ package nl.tudelft.dnainator.ui.controllers;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
@@ -13,11 +14,16 @@ import nl.tudelft.dnainator.ui.views.View;
  */
 public class ViewController {
 	private static final double ZOOM_FACTOR = 1e-3;
+	private static final int X_DELTA = 30;
+	private static final int Y_DELTA = 15;
+	private static final double SCROLLSPEED_INC = 0.3;
+	private static final int MAX_SCROLL_FACTOR = 4;
 
 	@FXML private View view;
 	@FXML private Group group;
 
 	private Point2D dragstart;
+	private double scrollSpeedFactor;
 
 	@FXML
 	private void onMouseDragged(MouseEvent e) {
@@ -32,6 +38,8 @@ public class ViewController {
 
 	@FXML
 	private void onMousePressed(MouseEvent e) {
+		scrollSpeedFactor = 1;
+		view.requestFocus();
 		if (e.getButton() == MouseButton.PRIMARY) {
 			dragstart = new Point2D(e.getX(), e.getY());
 		}
@@ -40,5 +48,33 @@ public class ViewController {
 	@FXML
 	private void onScroll(ScrollEvent e) {
 		view.zoom(e.getDeltaY() * ZOOM_FACTOR, new Point2D(e.getX(), e.getY()));
+	}
+
+	@FXML
+	private void onKeyPressed(KeyEvent e) {
+		scrollSpeedFactor = Math.min(scrollSpeedFactor + SCROLLSPEED_INC, MAX_SCROLL_FACTOR);
+		Point2D cur = new Point2D(view.getTranslateX(), view.getTranslateY());
+
+		switch (e.getCode()) {
+			case LEFT:
+				view.pan(cur.add(X_DELTA * scrollSpeedFactor, 0));
+				break;
+			case RIGHT:
+				view.pan(cur.add(-X_DELTA * scrollSpeedFactor, 0));
+				break;
+			case UP:
+				view.pan(cur.add(0, Y_DELTA * scrollSpeedFactor));
+				break;
+			case DOWN:
+				view.pan(cur.add(0, -Y_DELTA * scrollSpeedFactor));
+				break;
+			default:
+				break;
+		}
+	}
+
+	@FXML
+	private void onKeyReleased(KeyEvent e) {
+		scrollSpeedFactor = 1;
 	}
 }
